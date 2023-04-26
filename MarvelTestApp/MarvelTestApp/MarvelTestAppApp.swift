45cdd3808a7f082cfa13e3d75478897a0b36ad94
Leonardo Panta Leão <leonardopspl@gmail.com>
2023-04-26 16:22:08 -0300

    first commit.

---
+++ MarvelTestApp/MarvelTestApp/MarvelTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelTestAppApp.swift
+//  MarvelTestApp
+//
+//  Created by LEONARDO P S P LEAO on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
